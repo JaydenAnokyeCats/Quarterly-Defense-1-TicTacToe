c3dc80c36dda06f8d6e027ee04cec2cf0304b3ea
Jayden Anokye <jaydenanokyecats@gmail.com>
2025-10-06 10:35:29 -0400

    small additions, file creation, starting up the project

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class TicTacToe {
+public class TicTacToe {
+    // Variables
+    private String name;
+    private int Wins;
+    private int Losses;
+    private int Ties;
+    private int Streak;
+    private char[][] Grid;
+
+    //Default Constructor
+    public TicTacToe() {
+        name = null;
+        Wins = 0;
+        Losses = 0;
+        Ties = 0;
+        Streak = 0;
+        Grid = new char[3][3]; //This is how you declare a 2d char array
+    }
+
 }
