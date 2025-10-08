37f842a7273f5e222524b446ceb5d19f6c957c90
Jayden Anokye <jaydenanokye@webdev07.local>
2025-10-08 12:24:55 -0400

    added new methods, likely done with the tictactoe class, but may need a few additions

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,8 @@
 public class TicTacToe {
     // Variables
     private String name;
-    private int wins;
-    private int losses;
+    private double wins;
+    private double losses;
     private int ties;
     private int highscore;
     private char[][] Grid;
@@ -31,7 +31,50 @@ public class TicTacToe {
         System.out.printf("Losses: %s", losses);
         System.out.printf("Ties: %s", ties);
         System.out.printf("Highest Streak: %s", highscore);
+        System.out.printf("Win/Lose Ratio: %s", wins/losses);
     }
 
     //Mutators
+    //These mutators call whenever the player wins, loses, or ties.
+    public void addWins() {
+        wins += 1;
+    }
+
+    public void addLoss() {
+        losses += 1;
+    }
+
+    public void addTies() {
+        ties += 1;
+    }
+
+    //Calls whenever the player gets a new high score
+    public int getHighscore(int highscore) {
+        this.highscore = highscore;
+        return highscore;
+    }
+
+    //Board Creation Method
+    public String createBoard() {
+        StringBuilder sbBoard = new StringBuilder();
+
+        /* StringBuilder is a more effective method when dealing with strings.
+        For example, the method im using to connect multiple strings, append, is
+        a better way over simple concatenation.
+        Something I learned during my JUnit drills.
+         */
+
+        for (var i = 0; i < 3; i++) {
+            sbBoard.append("   ").append("|").append("   ").append("|\n");
+            for (var j = 0; j < 3; j++) {
+                if (j < 2){
+                    sbBoard.append("___|");
+                } else{
+                    sbBoard.append("___\n");
+                }
+            }
+        }
+
+        return sbBoard.toString();
+    }
 }
